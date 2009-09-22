48b0d36510a606f0e7647fba1a213fcbaddec7ef
welch <>
2009-09-22 00:43:48 +0000

    Added test_sessionException, test_invalidSession, test_fromId, test_param, test_params, test_destroy

--- perl/GridShibCA/test/CGISessionTest.pm
+++ perl/GridShibCA/test/CGISessionTest.pm
@@ -22,7 +22,12 @@ sub set_up
 {
     my $self = shift;
     $self->{session} = GridShibCA::CGISession->new();
-
+    $self->assert_not_null($self->{session});
+    $self->{session}->createNew();
+    $self->assert_not_null($self->{session}->id());
+    $self->{session}->param("Variable1", "Value1");
+    $self->{session}->param("Variable2", "Value2");
+    $self->{session}->flush();
 }
 
 sub tear_down
@@ -30,17 +35,58 @@ sub tear_down
     # Fixture cleanup
 }
 
-sub test_new
-{
-    my $self = shift;
-    $self->assert_not_null($self->{session});
-}
-
 sub test_sessionException
 {
     my $self = shift;
     $self->assert_raises(GridShibCA::SessionException,
 			 sub { throw GridShibCA::SessionException("test"); });
+}
+
+#
+# Check and make sure unititialized session throws an error if we try
+# to use it.
+#
+sub test_invalidSession
+{
+    my $self = shift;
+    my $session = GridShibCA::CGISession->new();
+    $self->assert_raises(GridShibCA::SessionException,
+			 sub { $session->id(); });
+}
+
+sub test_fromId
+{
+    my $self = shift;
+    my $id = $self->{session}->id();
+    my $session2 = GridShibCA::CGISession->new();
+    $self->assert_not_null($session2);
+    $session2->fromId($id);
+}
+
+sub test_param
+{
+    my $self = shift;
+
+    $self->assert_equals("Value1", $self->{session}->param("Variable1"));
+    $self->assert_equals("Value2", $self->{session}->param("Variable2"));
+}
+
+sub test_params
+{
+    my $self = shift;
+    my $params = $self->{session}->params();
+    $self->assert_not_null($params);
+    $self->assert_equals("Value1", $params->{Variable1});
+    $self->assert_equals("Value2", $params->{Variable2});
+}
+
+sub test_destroy
+{
+    my $self = shift;
+    $self->{session}->destroy();
+    # Should not be invalid to access
+    $self->assert_raises(GridShibCA::SessionException,
+			 sub { $self->{session}->id(); });
 }
 
 # Return true for import/use
